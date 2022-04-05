75f013536ea98403f3f2c066a5d510afed4a84b4
Diwas <diwasb54@gmail.com>
2022-04-05 07:46:11 +0545

    Searchable Method COnflict solve

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -49,8 +49,9 @@ class TaskRepository
         if (isset($data['id']) && !empty($data['id'])) {
           $this->task = $this->task->where('id', $data['id']);
         }
-        if ((isset($data['form_date']) && empty($data['form_date'])) || (isset($data['to_date']) && empty($data['to_date']))) {
-          $this->task  = $this->task->whereBetween('started_at', [$data['form_date'] ?? ' ', $data['to_date'] ?? ' ']);
+        if ((isset($data['form_date']) && !empty($data['form_date'])) || (isset($data['to_date']) && !empty($data['to_date']))) {
+
+            $this->task  = $this->task->whereBetween('started_at', [$data['form_date'] ?? ' ', $data['to_date'] ?? ' ']);
         }
         return $this->task->get() ;
     }
